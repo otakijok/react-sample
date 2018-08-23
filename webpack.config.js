9a9cad6596349cf423d0f5f7aae6ae1eaea2c118
otakijok <suprkoji@gmail.com>
2018-08-24 00:07:00 +0900

    途中だけどとりあえず。

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,28 @@ const path = require('path');
 
 module.exports = {
   entry: './src/js/app.js',
-  // 追加
   output: {
     path: path.resolve(__dirname, 'dist/js'),
     filename: 'bundle.js'
+  }
+  // 追加
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader:
+          options:
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.css']
   }
 };
