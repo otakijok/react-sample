036b38b4d5aa39606d069320e64a9af62924afff
otakijok <suprkoji@gmail.com>
2018-08-24 10:56:56 +0900

    きりのいいところまで。

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,12 @@
 // app.js
 import hello from './hello';
+import React from 'react';
+import ReactDom from 'react-dom';
+import style from '../css/style';
 
 hello();
+
+ReactDom.render(
+  <h1>Hello, Frontend Engineer!</h1>,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,43 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/js/app.js',
   output: {
     path: path.resolve(__dirname, 'dist/js'),
     filename: 'bundle.js'
-  }
-  // 追加
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader:
-          options:
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env', 'react']
+          }
         }
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+//        use: ['style-loader', 'css-loader']
       }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin('../css/style.css')
+  ],
   resolve: {
     extensions: ['.js', '.css']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/'),
+    historyApiFallback: true,
+    port: 3000
   }
 };
